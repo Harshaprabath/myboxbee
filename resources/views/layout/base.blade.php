c8a6c1be7d29f22bb270b19753e756463201c4b9
Harshaprabath <prabhathhemasiri@gmail.com>
2023-03-09 16:15:29 +0530

    coprategifting

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -34,6 +34,7 @@ $logo = slider::where('type', '=', 'logo')->get();
     <link rel="stylesheet" type="text/css" href="{{ asset('css/easyzoom.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/home.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/aboutus.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/coprategifting.css') }}">
     
 
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
